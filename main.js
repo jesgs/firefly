03cbd001f9217f66a661bad8cc20d99b959126ac
Jess Green <jgreen@psy-dreamer.com>
2015-05-12 21:01:19 -0500

    Moving global variables to main file.

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var Firefly = require('./firefly'),
-    tools   = require('./utilities');
+    tools   = require('./utilities'),
+    maxNumBugs = 100;
 
 
 var fireflyTimer = setInterval(runFirefly, 1000); // initial interval, changes while program is running
+
+Firefly.off(maxNumBugs);
 
 /**
  * Run Firefly program
@@ -12,18 +15,20 @@ function runFirefly()
     if (fireflyTimer) {
         clearInterval(fireflyTimer);
     }
-    
-    var interval = tools.getRandomArbitrary(3000, 20000),
+
+    Firefly.off(maxNumBugs);
+
+    var interval = tools.getRandomArbitrary(3000, 10000),
         fly = new Firefly();
 
     // attach parent interval for debugging
     fly.setParentInterval(interval);
-
+    fly.setPixel( tools.getRandomArbitrary(0, maxNumBugs) );
     fly.setup()
        .start()
        .pause()
        .debug();
-        
+
     // start a new timer
     fireflyTimer = setInterval(runFirefly, interval);
 }
